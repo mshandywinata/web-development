8f75f282fd2a40c83841005d9f2703b1d7b32692
mshandywinata <mshandywinata@upi.edu>
2026-04-03 11:06:10 +0700

    add server

---
+++ 5. express/4. secret/index.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import logger from './logger.js';
+import auth from './auth.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", auth, (req, res) => {
+    console.log(`Password authenticated: ${req.body.password}`);
+    res.sendFile(__dirname + "/public/secret.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on: http://localhost:${PORT}`);
+});
