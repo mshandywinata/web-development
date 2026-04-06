5ef380cd28e48ad14748208c165a8ca6c01fd6be
mshandywinata <mshandywinata@upi.edu>
2026-04-06 15:34:53 +0700

    add server instance

---
+++ 6. api/5. secret/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import morgan from "morgan";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+const BASE_URL = "https://secrets-api.appbrewery.com";
+
+app.use(morgan("common"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/random`);
+        const { secret, username } = response.data;
+
+        res.render("index.ejs", {
+            secret,
+            user: username,
+        });
+    } catch (error) {
+        console.log(`[ERROR] ${error.message}`);
+
+        res.render("index.ejs", {
+            secret: error.message,
+            user: null,
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}`);
+});
