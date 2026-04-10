f488deb5db0b2b50cd3283d0cc3bb515ef7aa9d2
mshandywinata <mshandywinata@upi.edu>
2026-04-10 19:17:37 +0700

    feat server routing

--- 7. database/3. postgres-read/index.js
+++ 7. database/3. postgres-read/index.js
@@ -16,7 +16,11 @@ const db = new pg.Client({
 
 db.connect();
 
+// for demonstration purpose
+// these values left as a global variable
 let flags = [];
+let currentQuestion = {};
+let currentScore = 0;
 
 db.query("SELECT * FROM flags", (err, res) => {
   if (err) {
@@ -28,9 +32,51 @@ db.query("SELECT * FROM flags", (err, res) => {
   db.end();
 });
 
+// after submit the currentQuestion going to be empty
+// hence async function is needed for this helper to populate the value
+const getQuestion = async () => {
+  const randomIndex = Math.floor(Math.random() * flags.length);
+  currentQuestion = flags[randomIndex];
+};
+
 app.use(morgan("common"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  currentScore = 0;
+  await getQuestion();
+
+  console.log(`[QUESTION]: ${currentQuestion.name}`);
+  console.log(`[SCORE]: ${currentScore}`);
+
+  res.status(200).render("index.ejs", { question: currentQuestion });
+});
+
+app.post("/submit", async (req, res) => {
+  const { answer } = req.body;
+  const inputAnswer = answer.trim().toLowerCase();
+  const trueAnswer = currentQuestion.name.toLowerCase();
+
+  let isCorrect = false;
+
+  if (inputAnswer === trueAnswer) {
+    currentScore++;
+    isCorrect = true;
+  }
+
+  await getQuestion();
+
+  console.log(`[QUESTION]: ${currentQuestion.name}`);
+  console.log(`[SCORE]: ${currentScore}`);
+
+  res.status(200).render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: currentScore,
+  });
+});
 
 app.listen(PORT, () => {
   console.info(`Server running on: http://localhost:${PORT}`);
