300f810d4007b506dcaa9e7f1e9025463dc12a8c
mshandywinata <mshandywinata@upi.edu>
2026-04-14 12:08:16 +0700

    add crud logic

--- 7. database/6. permalist/index.js
+++ 7. database/6. permalist/index.js
@@ -14,13 +14,97 @@ const db = new pg.Client({
   password: "mshandywinata",
   host: "localhost",
   port: 5432,
-  database: "",
+  database: "list",
 });
 
 db.connect();
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+const getListItems = async () => {
+  try {
+    const { rows } = await db.query("SELECT id, title FROM items");
+    return rows;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const addListItem = async (title) => {
+  try {
+    const added = await db.query("INSERT INTO items (title) VALUES ($1)", [
+      title,
+    ]);
+    return added;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const editListItem = async (id, title) => {
+  try {
+    const edited = await db.query("UPDATE items SET title = $1 WHERE id = $2", [
+      title,
+      id,
+    ]);
+    return edited;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const deleteListItem = async (id) => {
+  try {
+    const deleted = await db.query("DELETE FROM items WHERE id = $1", [id]);
+    return deleted;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const items = await getListItems();
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const { newItem } = req.body;
+  const inputTitle = newItem.trim();
+
+  try {
+    await addListItem(inputTitle);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const { updatedItemId, updatedItemTitle } = req.body;
+
+  try {
+    await editListItem(updatedItemId, updatedItemTitle);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const { deleteItemId } = req.body;
+
+  try {
+    await deleteListItem(deleteItemId);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.listen(PORT, () => {
