979b703fa6d039ae4d771a7c2b4db645d05329b6
mshandywinata <mshandywinata@upi.edu>
2026-04-14 18:52:17 +0700

    add crud routes

--- 7. database/7. book-notes/src/routes/bookRoutes.js
+++ 7. database/7. book-notes/src/routes/bookRoutes.js
@@ -1,7 +1,16 @@
 import express from "express";
+import {
+  createBookEntry,
+  deleteBookEntryById,
+  getAllBookEntries,
+  updateBookEntryById,
+} from "../controllers/bookController.js";
 
 const router = express.Router();
 
-router.get("/book", getAllBooks);
+router.get("/book", getAllBookEntries);
+router.post("/new", createBookEntry);
+router.patch("/book/:id", updateBookEntryById);
+router.delete("/book/:id", deleteBookEntryById);
 
 export default router;
