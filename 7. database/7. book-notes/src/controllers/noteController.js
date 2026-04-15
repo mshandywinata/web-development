62a5e1e6c6e872767e95f6684e5d7bcf82d8150f
mshandywinata <mshandywinata@upi.edu>
2026-04-15 11:20:18 +0700

    feat controller bussiness logic moved into service

--- 7. database/7. book-notes/src/controllers/noteController.js
+++ 7. database/7. book-notes/src/controllers/noteController.js
@@ -1,33 +1,50 @@
-import prisma from "../lib/prisma.js";
+import {
+  findAllNotes,
+  modifyNote,
+  removeNote,
+  saveNote,
+} from "../services/noteService.js";
 
-const model = prisma.note;
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await findAllNotes();
 
-export const getAllNoteEntries = async () => {
-  return await model.findMany({
-    orderBy: {
-      [sortBy]: order,
-    },
-    include: {
-      notes: true,
-    },
-  });
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch all notes." });
+  }
 };
 
-export const createNoteEntry = async (note) => {
-  return await model.create({
-    data: note,
-  });
+export const createNote = async (req, res) => {
+  try {
+    const { content } = req.body;
+    const createdNote = await saveNote(content);
+
+    res.json(createdNote);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to save new note." });
+  }
 };
 
-export const updateNoteEntryById = async (id, note) => {
-  return await model.update({
-    where: { id },
-    data: note,
-  });
+export const updateNote = async (req, res) => {
+  try {
+    const { content } = req.body;
+    const { id } = req.params;
+    const updatedNote = await modifyNote(id, content);
+
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to modify note." });
+  }
 };
 
-export const deleteNoteEntryById = async (id) => {
-  return await model.delete({
-    where: { id },
-  });
+export const deleteNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedNote = await removeNote(id);
+
+    res.json(deletedNote);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to remove note." });
+  }
 };
