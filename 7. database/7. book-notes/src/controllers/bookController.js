6232e55720b89e2da983589fa0160301a5804d3f
mshandywinata <mshandywinata@upi.edu>
2026-04-15 11:19:38 +0700

    feat book controller bussiness logic moved into servicees

--- 7. database/7. book-notes/src/controllers/bookController.js
+++ 7. database/7. book-notes/src/controllers/bookController.js
@@ -1,42 +1,64 @@
-import prisma from "../lib/prisma.js";
+import {
+  findAllBooks,
+  findBookById,
+  modifyBook,
+  removeBook,
+  saveBook,
+} from "../services/bookService.js";
 
-const model = prisma.book;
+export const getAllBooks = async (req, res) => {
+  try {
+    const sortBy = req.query.sortBy || "created_at";
+    const order = req.query.order || "desc";
+    const books = await findAllBooks(sortBy, order);
 
-export const getAllBookEntries = async (
-  sortBy = "created_at",
-  order = "desc",
-) => {
-  return await model.findMany({
-    orderBy: {
-      [sortBy]: order,
-    },
-    include: {
-      notes: true,
-    },
-  });
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch books." });
+  }
 };
 
-export const getBookEntryById = async (id) => {
-  return await model.findUnique({
-    where: { id: parseInt(id) },
-  });
+export const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await findBookById(parseInt(id));
+
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch book by id." });
+  }
 };
 
-export const createBookEntry = async (book) => {
-  return await model.create({
-    data: book,
-  });
+export const createBook = async (req, res) => {
+  try {
+    const book = req.body;
+    const createdBook = await saveBook(book);
+
+    res.json(createdBook);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to save new book." });
+  }
 };
 
-export const updateBookEntryById = async (id, book) => {
-  return await model.update({
-    where: { id },
-    data: book,
-  });
+export const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = req.body;
+    const updatedBook = await modifyBook(id, book);
+
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to modify book." });
+  }
 };
 
-export const deleteBookEntryById = async (id) => {
-  return await model.delete({
-    where: { id },
-  });
+export const deleteBook = async (id) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await removeBook(id);
+
+    res.json(deletedBook);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to remove book." });
+  }
 };
