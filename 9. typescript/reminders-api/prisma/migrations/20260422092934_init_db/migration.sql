2619c3f4a0b326f1013c920c10d0e1fd6d39a4ea
mshandywinata <mshandywinata@upi.edu>
2026-04-22 18:32:53 +0700

    init db

---
+++ 9. typescript/reminders-api/prisma/migrations/20260422092934_init_db/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Reminder" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Reminder_pkey" PRIMARY KEY ("id")
+);
